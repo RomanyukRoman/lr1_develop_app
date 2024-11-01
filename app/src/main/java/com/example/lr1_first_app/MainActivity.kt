f75e08ab6dfa83703cec443ca3ed21605915c091
RomanRomanyuk <romaniuk.roman1121@vu.cdu.edu.ua>
2024-11-01 12:49:47 +0200

    change_button

--- app/src/main/java/com/example/lr1_first_app/MainActivity.kt
+++ app/src/main/java/com/example/lr1_first_app/MainActivity.kt
@@ -1,16 +1,14 @@
 package com.example.lr1_first_app
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.viewModels
 import com.example.lr1_first_app.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
 
-    private val textViewModel: MainViewModel by viewModels()
-    private lateinit var binding : ActivityMainBinding
+        private val textViewModel: MainViewModel by viewModels()
+        private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +21,7 @@ class MainActivity : ComponentActivity() {
             binding.textView1.text = newText
         }
         binding.button1.setOnClickListener {
-            val newText = "Change text"
-            textViewModel.changeText(newText)
+            textViewModel.changeText("Change text")
         }
     }
 }
